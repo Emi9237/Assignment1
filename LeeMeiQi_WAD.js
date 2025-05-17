e8b2acdee0555917bfee740fd8bed97dd7a42442
Emi9237 <emilialee447@gmail.com>
2025-05-17 22:43:06 +0800

    Added the description, items and payment methods of the store

--- LeeMeiQi_WAD.js
+++ LeeMeiQi_WAD.js
@@ -1,10 +1,15 @@
 module.exports = {
-    // Explain what function A does
-    functionA() {
-        return 1+2;
-    },
-    // Explain what function B does
-    functionB() {
-        console.log("Hello function B");
-    }
+    description : "Welcome to Shopee!",
+    items : [
+        "T-shirt, $8", 
+        "Blouse, $12",
+        "Pants, $9.90",
+        "Skirt, $10.90",
+        "Dress, $14"
+    ],
+    methods :[
+        "PayNow",
+        "Google Pay",
+        "Credit Card/Debit Card"
+    ],
 }
